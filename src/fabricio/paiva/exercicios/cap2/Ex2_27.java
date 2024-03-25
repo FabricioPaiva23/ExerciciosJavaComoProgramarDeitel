b16740be26a7578a2c5a4dcbbb7107602db1d591
fabricio <cdpaiva24530@gmail.com>
2024-03-25 20:14:09 -0300

    Exercicio 2.27 do cap 2

---
+++ src/fabricio/paiva/exercicios/cap2/Ex2_27.java
@@ -0,0 +1,10 @@
+package fabricio.paiva.exercicios.cap2;
+
+public class Ex2_27 {
+    public static void main(String[] args) {
+        System.out.print("*  *  *  *  *  *  *  *  *\n  *  *  *  *  *  *  *  *  *\n");
+        System.out.print("*  *  *  *  *  *  *  *  *\n  *  *  *  *  *  *  *  *  *\n");
+        System.out.print("*  *  *  *  *  *  *  *  *\n  *  *  *  *  *  *  *  *  *\n");
+        System.out.print("*  *  *  *  *  *  *  *  *\n  *  *  *  *  *  *  *  *  *\n");
+    }
+}
