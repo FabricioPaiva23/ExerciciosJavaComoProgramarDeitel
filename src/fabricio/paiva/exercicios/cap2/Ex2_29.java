3ba58741c8b77578b3576f5a447803601546c700
fabricio <cdpaiva24530@gmail.com>
2024-03-25 20:15:53 -0300

    Exercicio 2.29 do cap 2

---
+++ src/fabricio/paiva/exercicios/cap2/Ex2_29.java
@@ -0,0 +1,20 @@
+package fabricio.paiva.exercicios.cap2;
+
+public class Ex2_29 {
+    public static void main(String[] args) {
+        System.out.printf("%c equivale a %d%n", 'A', ((int)'A'));
+        System.out.printf("%c equivale a %d%n", 'B', ((int)'B'));
+        System.out.printf("%c equivale a %d%n", 'C', ((int)'C'));
+        System.out.printf("%c equivale a %d%n", 'a', ((int)'a'));
+        System.out.printf("%c equivale a %d%n", 'b', ((int)'b'));
+        System.out.printf("%c equivale a %d%n", 'c', ((int)'c'));
+        System.out.printf("%c equivale a %d%n", '0', ((int)'0'));
+        System.out.printf("%c equivale a %d%n", '1', ((int)'1'));
+        System.out.printf("%c equivale a %d%n", '2', ((int)'2'));
+        System.out.printf("%c equivale a %d%n", '$', ((int)'$'));
+        System.out.printf("%c equivale a %d%n", '*', ((int)'*'));
+        System.out.printf("%c equivale a %d%n", '+', ((int)'+'));
+        System.out.printf("%c equivale a %d%n", '/', ((int)'/'));
+        System.out.printf("%c equivale a %d%n", ' ', ((int)' '));
+    }
+}
