dc0e6b7dc48d0c633063dd76e5c6af517113a1dc
fabricio <cdpaiva24530@gmail.com>
2024-03-25 20:08:35 -0300

    Exercicio 2.18 do cap 2

---
+++ src/fabricio/paiva/exercicios/cap2/Ex2_18.java
@@ -0,0 +1,15 @@
+package fabricio.paiva.exercicios.cap2;
+
+public class Ex2_18 {
+    public static void main(String[] args) {
+        System.out.printf("%s%s%s%s%s%s%s%s%s\t\t%s%s%s\t\t %s\t\t %s%n","*","*","*","*","*","*","*","*","*","*","*","*","*","*");
+        System.out.printf("%s\t\t%s\t  %s\t\t%s   %s%s%s\t\t%s %s%n", "*","*","*","*","*","*","*","*","*");
+        System.out.printf("%s\t\t%s\t %s\t\t %s %s%s%s%s%s   %s   %s%n", "*","*","*","*","*","*","*","*","*","*", "*");
+        System.out.printf("%s\t\t%s\t %s\t\t %s\t %s    %s     %s%n", "*","*","*","*","*","*","*");
+        System.out.printf("%s\t\t%s\t %s\t\t %s\t %s   %s       %s%n", "*","*","*","*","*","*","*");
+        System.out.printf("%s\t\t%s\t %s\t\t %s\t %s    %s     %s%n", "*","*","*","*","*","*","*");
+        System.out.printf("%s\t\t%s\t %s\t\t %s\t %s     %s   %s%n", "*","*","*","*","*","*","*");
+        System.out.printf("%s\t\t%s\t  %s\t\t%s\t %s      %s %s%n", "*","*","*","*","*","*","*");
+        System.out.printf("%s%s%s%s%s%s%s%s%s\t\t%s%s%s\t\t %s\t\t %s%n","*","*","*","*","*","*","*","*","*","*","*","*","*","*");
+    }
+}
