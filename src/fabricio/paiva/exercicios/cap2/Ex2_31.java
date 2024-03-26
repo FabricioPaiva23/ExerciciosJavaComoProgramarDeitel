44fba1eebabd25ee545344553d09e4eb5ac383f6
fabricio <cdpaiva24530@gmail.com>
2024-03-26 09:46:09 -0300

    Exercicio 2.31 do cap 2

---
+++ src/fabricio/paiva/exercicios/cap2/Ex2_31.java
@@ -0,0 +1,18 @@
+package fabricio.paiva.exercicios.cap2;
+
+public class Ex2_31 {
+    public static void main(String[] args) {
+        System.out.println("number\tsquare\tcube");
+        System.out.printf("%d\t\t%d\t\t%d\n", 0, 0, 0);
+        System.out.printf("%d\t\t%d\t\t%d\n", 1, (int)Math.pow(1 ,2), (int)Math.pow(1, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 2, (int)Math.pow(2 ,2), (int)Math.pow(2, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 3, (int)Math.pow(3 ,2), (int)Math.pow(3, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 4, (int)Math.pow(4 ,2), (int)Math.pow(4, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 5, (int)Math.pow(5 ,2), (int)Math.pow(5, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 6, (int)Math.pow(6 ,2), (int)Math.pow(6, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 7, (int)Math.pow(7 ,2), (int)Math.pow(7, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 8, (int)Math.pow(8 ,2), (int)Math.pow(8, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 9, (int)Math.pow(9 ,2), (int)Math.pow(9, 3));
+        System.out.printf("%d\t\t%d\t\t%d\n", 10, (int)Math.pow(10 ,2), (int)Math.pow(10, 3));
+    }
+}
